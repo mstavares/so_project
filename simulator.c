17b3281b449aa64db2a163071a85f1e1e1c5464d
mstavares <hmigueltavares@gmail.com>
2016-10-17 20:06:58 +0100

    algoritmo de precedencia em desenvolvimento

--- simulator.c
+++ simulator.c
@@ -18,9 +18,94 @@
 #include <signal.h>
 #include "transaction.h"
 
-#define NUMBER_OF_THREADS 1
+//#define NUMBER_OF_THREADS 1
 #define NUMBER_OF_PIPES 4
 
+
+transaction_t* waiting_list[NUMBER_OF_PIPES];
+
+#define NUMBER_OF_THREADS 3
+int array[4];
+
+int compare(int a, int b) {
+	if(a < b) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+
+/*
+int waiting_algorithm() {
+	int val;
+	if(compare(waiting_list[0]->timestamp, waiting_list[1]->timestamp)) {
+		if(compare(waiting_list[0]->timestamp, waiting_list[2]->timestamp)) {
+			if(compare(waiting_list[0]->timestamp, waiting_list[3]->timestamp)) {
+				val = 0;
+			} else {
+				val = 3;
+			}
+		} else {
+			if(compare(waiting_list[2]->timestamp, waiting_list[3]->timestamp)) {
+				val = 2;
+			} else {
+				val = 3;
+			}
+		}
+	} else {
+		if(compare(waiting_list[1]->timestamp, waiting_list[2]->timestamp)) {
+			if(compare(waiting_list[1]->timestamp, waiting_list[3]->timestamp)) {
+				val = 1;
+			} else {
+				val = 3;
+			}
+		} else {
+			if(compare(waiting_list[2]->timestamp, waiting_list[3]->timestamp)) {
+				val = 2;
+			} else {
+				val = 3;
+			}
+		}
+	}
+	return val;
+}
+*/
+
+
+int waiting_algorithm() {
+	int val;
+	if(compare(array[0], array[1])) {
+		if(compare(array[0], array[2])) {
+			if(compare(array[0], array[3])) {
+				val = 0;
+			} else {
+				val = 3;
+			}
+		} else {
+			if(compare(array[2], array[3])) {
+				val = 2;
+			} else {
+				val = 3;
+			}
+		}
+	} else {
+		if(compare(array[1], array[2])) {
+			if(compare(array[1], array[3])) {
+				val = 1;
+			} else {
+				val = 3;
+			}
+		} else {
+			if(compare(array[2], array[3])) {
+				val = 2;
+			} else {
+				val = 3;
+			}
+		}
+	}
+	return val;
+}
 
 void *readOrders() {
 	int i;
@@ -41,23 +126,52 @@ void *readOrders() {
 		}
 		
 		fread(transaction, sizeof(transaction_t), 1, fifos[i]);
-		printf("%d: %s \n", i, print_transaction(transaction));
+		//printf("%d: %s \n", i, print_transaction(transaction));
 
 	}
 	
 	//fclose(r);
 }
 
+int randomize() {
+	return rand() % 20;
+}
+
+void teste_algoritmo() {
+	int i;
+	for(i = 0; i < 4; i++) {
+		array[i] = randomize();
+	}
+	printf("MENOR: %d \n", waiting_algorithm());
+}
+
+void ultima() {
+	int i;
+	teste_algoritmo();
+	printf("---------------------\n");
+	for(i = 0; i < 4; i++) {
+		printf("%d -> %d \n", i, array[i]);
+	}
+}
 
 int main(int argc, char* argv[]) {
 	int i;
 	pthread_t threads[NUMBER_OF_THREADS];
 	
+	for(i = 0; i < 5; i++) {
+		ultima();
+    }	
+    
+    	
+    	
+    	
+	/*
 	for(i = 0; i < NUMBER_OF_THREADS; i++) {
 		pthread_create(&threads[i], NULL, readOrders, NULL);
 	}
 
     for(i = 0; i < NUMBER_OF_THREADS; i++) {
 		pthread_join(threads[i], NULL);
-    }	
+    }
+    */
 }
